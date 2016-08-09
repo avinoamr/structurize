6940306f59aed4b580a771df91aaf9e763456cff
Alon Brody <alon.brody@gmail.com>
2016-08-09 12:56:04 +0300

    Added the ability to decide what will be the delimiter

--- formats/csv.js
+++ formats/csv.js
@@ -1,16 +1,20 @@
 module.exports.type = "csv";
+var DEFAULT_DELIMITER = ','
 
-module.exports.is = function isCSV( sample ) {
-    return sample.toString().split( "\n" )[ 0 ].indexOf( "," ) != -1;
+module.exports.is = function isCSV( sample, delimiter ) {
+    var useDelimiter = delimiter || DEFAULT_DELIMITER;
+    return sample.toString().split( "\n" )[ 0 ].indexOf( useDelimiter ) != -1;
 }
 
-module.exports.parser = function () {
+module.exports.parser = function ( delimiter ) {
+    var useDelimiter = delimiter || DEFAULT_DELIMITER;
     var csvparse = require( "csv-parse" );
     return csvparse({ 
         skip_empty_lines: true, 
         escape: "\\", 
         columns: true,
         relax: true, 
+        delimiter: useDelimiter
     })
 }
 
