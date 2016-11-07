4b033a6b1931a87e4165f2046bd41a8cea13fec2
kfir <kfirgez@gmail.com>
2016-11-07 12:29:43 +0200

    use wdl stream as the parser for wdl

--- formats/wdl.js
+++ formats/wdl.js
@@ -2,9 +2,15 @@ module.exports.type = "WDL";
 var INDEX0_SIG = "#Version:";
 var INDEX1_SIG = "#Fields:"
 
-module.exports.is = function isWDL( sample, options ) {
+module.exports.is = function isWDL( sample ) {
     options = options || {};
     var result = sample.toString().split( "\n" );
     return (result[0].indexOf(INDEX0_SIG) > -1 && 
             result[1].indexOf(INDEX1_SIG) > -1);
 }
+
+module.exports.parser = function () {
+    return require( "wdl-stream" )();
+}
+
+module.exports.requires = [ "wdl-stream" ]
