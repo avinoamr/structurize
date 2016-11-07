d241b493f99970744f0e221e0fe9672b4fdd634a
kfir <kfirgez@gmail.com>
2016-11-07 12:16:12 +0200

    detect WDL format

--- formats/wdl.js
+++ formats/wdl.js
@@ -1,8 +1,10 @@
 module.exports.type = "WDL";
+var INDEX0_SIG = "#Version:";
+var INDEX1_SIG = "#Fields:"
 
 module.exports.is = function isWDL( sample, options ) {
     options = options || {};
     var result = sample.toString().split( "\n" );
-    console.log('^^^^^^^^^^^^LOOK:', result);
-    return false;
+    return (result[0].indexOf(INDEX0_SIG) > -1 && 
+            result[1].indexOf(INDEX1_SIG) > -1);
 }
