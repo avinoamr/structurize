e6547677a7144ddaf006869d94f1ef8c6dfcd65d
kfir <kfirgez@gmail.com>
2016-11-07 12:19:36 +0200

    test negation

--- formats/wdl.js
+++ formats/wdl.js
@@ -5,6 +5,6 @@ var INDEX1_SIG = "#Fields:"
 module.exports.is = function isWDL( sample, options ) {
     options = options || {};
     var result = sample.toString().split( "\n" );
-    return (result[0].indexOf(INDEX0_SIG) > -1 && 
+    return !(result[0].indexOf(INDEX0_SIG) > -1 && 
             result[1].indexOf(INDEX1_SIG) > -1);
 }
