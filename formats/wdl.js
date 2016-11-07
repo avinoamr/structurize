b9cbb526147bd8a0ebf8c1d2b50965f7ac95bbbf
kfir <kfirgez@gmail.com>
2016-11-07 12:39:30 +0200

    remove options

--- formats/wdl.js
+++ formats/wdl.js
@@ -3,7 +3,6 @@ var INDEX0_SIG = "#Version:";
 var INDEX1_SIG = "#Fields:"
 
 module.exports.is = function isWDL( sample ) {
-    options = options || {};
     var result = sample.toString().split( "\n" );
     return (result[0].indexOf(INDEX0_SIG) > -1 && 
             result[1].indexOf(INDEX1_SIG) > -1);
