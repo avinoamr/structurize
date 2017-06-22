c0a74cd36cae68342ac7b9537d877294224f7739
herzaso <herzaso@gmail.com>
2017-06-22 10:30:59 +0300

    fix csv with 1 column should work

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ function structurize ( options ) {
     return new Stream( options );
 }
 
+var csvModule = require( "./formats/csv" )
+
 structurize.formats = [
     require( "./formats/xlsx" ),
     require( "./formats/tar" ),
     require( "./formats/gzip" ),
     require( "./formats/json" ),
-    require( "./formats/csv" ),
+    csvModule,
     require( "./formats/tsv" ),
     require( "./formats/querystring" ),
     require( "./formats/wdl" ),
@@ -27,6 +29,14 @@ structurize.guess = function ( sample, options ) {
             return this.formats[ i ].type;
         }
     }
+    // if no format is suitable and sample has new lines,
+    // consider it as a csv with 1 column
+    // this check must be the last check since we don't want every sample with
+    // new lines to be considered as csv (only if no other option is valid).
+    // Also, we can't move the csv test to the end since the list must be
+    // ordered by relevancy/statistics
+    if (sample.toString().indexOf("\n") >= 0)
+        return csvModule.type
 }
 
 structurize.parser = function ( type, options ) {
